09e19646ad6d34cd0e8bc6702435e7d6ad6c5687
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-11-19 11:57:53 +0000

    Added rounting for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "hello_koyeb/index"
+
+  root "hello_world#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :products
 
