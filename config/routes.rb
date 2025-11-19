a534a61256102864cb2a68edd46228a1023122e1
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-11-19 12:02:42 +0000

    Added RETful rounting

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get "hello_koyeb/index"
 
-  root "hello_world#index"
+  # root "hello_world#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # resources :products
+  resources :products
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
