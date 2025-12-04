ce5800ef937cb00e7bc62d884900ac5907c3de9a
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-12-04 13:57:14 +0000

    Created migration for test

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_18_132716) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_19_144318) do
   create_table "products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
